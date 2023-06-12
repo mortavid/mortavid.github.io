1ec51c873f117ded0f3e52085896f55dc1c1830e
mortavid <136274858+mortavid@users.noreply.github.com>
2023-06-12 13:58:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+body {
+    background-color: #000000;
+    color: #ffffff;
+    font-family: "Old London", cursive;
+  }
+  
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+  
+  .logo {
+    font-size: 48px;
+    margin-bottom: 20px;
+  }
+  
+  .coming-soon {
+    font-size: 24px;
+    margin-bottom: 10px;
+  }
+  
+  .brand-story {
+    font-size: 16px;
+    text-align: center;
+  }
+  
